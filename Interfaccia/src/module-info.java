7b6d4a49aaf7d693427d78590c3cdc86161143ab
Fochi1999 <73483730+Fochi1999@users.noreply.github.com>
2022-12-10 15:14:46 +0100

    Delete Interfaccia directory

--- Interfaccia/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module Interfaccia {
-	requires javafx.controls;
-	requires java.sql;
-	requires com.google.common;
-	requires javafx.graphics;
-	opens application to javafx.graphics, javafx.fxml;
-}
